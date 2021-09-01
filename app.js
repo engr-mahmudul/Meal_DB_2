30e714680693fc5643b7fb2e37dafa67b9932ece
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-01 10:38:08 +0600

    fetch url

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
+let showData = (data) =>{
+    console.log(data);
+}
+let fetchUrlWithSerachText = (searchText) =>{
+    let url;
+    if(searchText.length === 1){
+        console.log("seaching by first letter");
+        url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchText}`;
+    }
+    else if(searchText.length>1){
+        console.log("seaching by name");
+        url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+
+    }
+    else{
+        const messageField = document.getElementById('messages');
+        let div = document.createElement('div');
+        div.classList.add("bg-danger","p-3","container");
+        div.innerHTML = `
+        <p class="text-white text-center fw-bold fs-5 text"> Please put a Food name for searching</p>
+        `
+        messageField.appendChild(div);
+    }
+
+    fetch(url)
+    .then(respones => respones.json())
+    .then(data=> showData(data.meals));
+}
 document.getElementById('search-button').addEventListener('click',function(){
     let searchField = document.getElementById('search-field');
-    se
+    let searchText = searchField.value;
+    // //let data = fetchUrlWithSerachText(searchText);
+    // console.log(data);
+    fetchUrlWithSerachText(searchText);
 
     searchField.value = '';
 });
