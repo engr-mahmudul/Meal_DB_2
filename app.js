5d580503f6fb979b8a771c94e6123b1729f15fa4
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-01 09:50:59 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById('search-button').addEventListener('click',function(){
+    let searchField = document.getElementById('search-field');
+    se
+
+    searchField.value = '';
+});
